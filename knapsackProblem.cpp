f16f829dec7bea52e2dc02ab9327fb372cec519c
roshan22418 <roshan22418@iiitd.ac.in>
2024-01-22 12:34:42 +0530

    add some file in longest common Subsequece

--- knapsackProblem.cpp
+++ knapsackProblem.cpp
@@ -2,43 +2,144 @@
 #include <vector>
 
 using namespace std;
+int solveusingTabulation(int *profit,int *wt,int &capacity,int length){
+    vector<vector<int>> dp(capacity+1,vector<int> (length+1,-1));
+    //according to base case in recursion and memization use only;
+    for (int row = 0; row <= capacity; row++)
+    {
+        dp[row][length]=0;
+    }
+    for (int i = 0; i <= capacity; i++)
+    {
+        //i do wrong thing in looping condition 
+        for (int j = length-1; j >= 0; j--)
+        {
+            int include = 0;
+            if (i>=wt[j])
+            {
+                // and here forget subtract capacity of corresponding weight in row;
+                include = profit[j] + dp[i-wt[j]][j+1];
+            }
+            int exlude = 0+ dp[i][j+1];
+            dp[i][j] = max(include,exlude);
+            
+            
+        }
+        
+    }
+    return dp[capacity][0];
+    
+}
 
-int solve(int *profit, int *wt, int index, int length, int &capacity,vector<vector<int>> &dp)
-{
-    // if (index == length - 1)
-    // {
-    //     if (wt[index] <= capacity)
-    //     {
-    //         return profit[index];
-    //         /* code */
-    //     }
-    //     else
-    //     {
-    //         return 0;
-    //     }
+
+int solveUSingSpaceOptimization(int *profit,int *wt,int capacity,int length){
+    vector<int> next(capacity+1,-1);
+    vector<int> curr(capacity+1,-1);
+    for (int row = 0; row <= capacity+1; row++)
+    {
+        next[row]  = 0;
+    }
+    for (int j = length-1; j >= 0; j--)
+    {
+        for(int i  = 0;i<=capacity;i++)        
+        {
+            int include = 0;
+            if(i>=wt[j]){
+                include = profit[j] + next[i-wt[j]];
+
+
+            }
+            int exlude = 0 + next[i];
+            curr[i] = max(include,exlude);
+        }
+        next = curr;
+        
+    }
+    return curr[capacity];
+    
+    
+}
+
+
+// int solve(int *profit, int *wt, int index, int length, int &capacity,vector<vector<int>> &dp)
+// {
+//     // if (index == length - 1)
+//     // {
+//     //     if (wt[index] <= capacity)
+//     //     {
+//     //         return profit[index];
+//     //         /* code */
+//     //     }
+//     //     else
+//     //     {
+//     //         return 0;
+//     //     }
+//     // }
+
+//     if(index>=length){
+//         return 0;
+//     }
+     
+//     if(dp[capacity][index]!=-1){
+//         return dp[capacity][index];
+//     }
+//      int include = 0;
+//      if (wt[index]<=capacity)
+//      {
+//         int remainingCapacity = capacity - wt[index]; 
+//         include = profit[index] + solve(profit,wt,index+1,length,remainingCapacity,dp);
+
+       
+//      }   
+    
+//     int exclude = 0+ solve(profit,wt,index+1,length,capacity,dp);
+//     dp[capacity][index] = max(include,exclude);
+//     return dp[capacity][index]; 
+// }
+
+
+
+int solve(int *profit,int *wt,int index,int length,int &capacity,vector<vector<int>> &dp){
+    if (index>=length) 
+    {
+        return 0;
+    }
+    if(dp[capacity][index]!=-1){
+        return  dp[capacity][index];
+    }
+    int include = 0;
+    if (wt[index]<=capacity)    
+    {
+        int remainingCapacity = capacity - wt[index];
+        include =profit[index] +  solve(profit,wt,index+1,length,remainingCapacity,dp);
+    }
+    int exclude = 0+ solve(profit,wt,index+1,length,capacity,dp);
+    dp[capacity][index] = max(include,exclude);
+    return dp[capacity][index];
+
+    
+}
+int solveUsingRecurssion(int *profit,int *wt,int index,int length,int &capacity){
+    //i am stuck here why this is not base case; may be if capacity is not zero but index out of bound means increase by length 
+
+    // if(capacity<=0){
+    //     return 0;
     // }
 
     if(index>=length){
         return 0;
     }
-     
-    if(dp[capacity][index]!=-1){
-        return dp[capacity][index];
+    int include = 0;
+    if (wt[index]<=capacity)
+    {
+        int remainingCapacity = capacity - wt[index];
+        include = profit[index] + solveUsingRecurssion(profit,wt,index+1,length,remainingCapacity);
     }
-     int include = 0;
-     if (wt[index]<=capacity)
-     {
-        int remainingCapacity = capacity - wt[index]; 
-        include = profit[index] + solve(profit,wt,index+1,length,remainingCapacity,dp);
-
-       
-     }   
+    int exlude = 0 + solveUsingRecurssion(profit,wt,index+1,length,capacity);
+    int ans  = max(include,exlude);
+    return ans;
     
-    int exclude = 0+ solve(profit,wt,index+1,length,capacity,dp);
-    dp[capacity][index] = max(include,exclude);
-    return dp[capacity][index]; 
 }
-
 int main()
 {
     int profit[] = {60, 100, 120};
@@ -47,7 +148,10 @@ int main()
     int length = 3;
     int capacity = 50;
     vector<vector<int>> dp(capacity+1,vector<int>(length+1,-1)); 
-    int ans = solve(profit, wt, index, length, capacity,dp);
+    // int ans = solve(profit, wt, index, length, capacity,dp);
+    // int ans  = solveUsingRecurssion(profit,wt,index,length,capacity);
+    // int ans = solveusingTabulation(profit,wt,capacity,length);
+    int ans = solveUSingSpaceOptimization(profit,wt,capacity,length);
     cout<<ans<<" ";
 
     return 0;
---
+++ longestCommSUbsequece.cpp
@@ -0,0 +1,120 @@
+
+
+
+
+
+
+
+#include<iostream>
+using namespace std;
+#include<vector>
+
+
+
+
+class Solution {
+public:
+    int solveUsingSpaceOptimization(string text1,string text2){
+        int length1 = text1.length();
+        int length2 = text2.length();
+        vector<int> next(length1+1,0);
+        vector<int> curr(length1+1,0);
+        for(int i = length1-1;i>=0;i--){
+            for(int j = length2-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    curr[i][j] = 1+ next[i+1][j+1];
+                }
+
+        //why add else statement in this case this thing i am not understand 
+                else{
+                    int notMatch1 = dp[i][j+1];
+                    int notMatch2 = dp[i+1][j];
+                    dp[i][j] = max(notMatch1,notMatch2);
+                }
+
+            }
+        }
+        return dp[0][0]; 
+
+    }
+
+
+
+    }
+    int solveusingTabulation(string text1,string text2){
+        int length1 = text1.length();
+        int length2 = text2.length();
+        vector<vector<int>> dp(length1+1,vector<int>(length2+1,-1));
+        for(int i = 0;i<=length2;i++){
+            dp[length1][i] = 0;
+        }
+        for(int i = 0;i<=length1;i++){
+            dp[i][length2] = 0;
+        }
+        for(int i = length1-1;i>=0;i--){
+            for(int j = length2-1;j>=0;j--){
+                if(text1[i]==text2[j]){
+                    dp[i][j] = 1+ dp[i+1][j+1];
+                }
+
+        //why add else statement in this case this thing i am not understand 
+                else{
+                    int notMatch1 = dp[i][j+1];
+                    int notMatch2 = dp[i+1][j];
+                    dp[i][j] = max(notMatch1,notMatch2);
+                }
+
+            }
+        }
+        return dp[0][0]; 
+
+    }
+
+    int solveUSingRecursion(string text1,string text2,int i ,int j){
+        if(i>=text1.length() || j>= text2.length()){
+            return 0;
+        }
+        int ans = 0;
+        if(text1[i]==text2[j]){
+            ans = 1+ solveUSingRecursion(text1,text2,i+1,j+1);
+        }
+
+        //why add else statement in this case this thing i am not understand 
+        else{
+        int notMatch1 = solveUSingRecursion(text1,text2,i,j+1);
+        int notMatch2 = solveUSingRecursion(text1,text2,i+1,j);
+        ans = max(notMatch1,notMatch2);
+        }
+        return ans; 
+    }
+
+    int solveusingMEM(string text1,string text2,int i,int j,vector<vector<int>> &dp){
+        if(i>=text1.length() || j>= text2.length()){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(text1[i]==text2[j]){
+            dp[i][j] = 1 + solveusingMEM(text1,text2,i+1,j+1,dp);
+        }
+        else{
+            int exlude1 = 0 + solveusingMEM(text1,text2,i,j+1,dp);
+            int exlude2 = 0 + solveusingMEM(text1,text2,i+1,j,dp);
+            dp[i][j] = max(exlude1,exlude2);
+        }
+        return dp[i][j];
+        
+        
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        int i = 0;
+        int j = 0;
+        int length1 = text1.length();
+        int length2 = text2.length();
+        int ans = solveusingTabulation(text1,text2);
+        return ans ;
+        
+    }
+};
+
