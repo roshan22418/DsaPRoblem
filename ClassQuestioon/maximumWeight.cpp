7dd41d90fe2f69eda2946f2836c75842873a031e
roshan22418 <roshan22418@iiitd.ac.in>
2024-01-24 19:20:30 +0530

    add all way

--- ClassQuestioon/maximumWeight.cpp
+++ ClassQuestioon/maximumWeight.cpp
@@ -17,14 +17,47 @@ int solve(int *arr,int i ,int n,vector<int> &ans1){
 
 }
 
+int solveusingtabulationTowDown(int *arr,int length){
+    vector<int> ans(length+1,0);
+    ans[length] = 0;
+    ans[length-1] = arr[length-1];
+    for (int i = length-2; i >=0; i--)
+    {
+        int include = arr[i] + ans[i+2];
+        int exlude = 0 + ans[i+1];
+        ans[i] = max(include,exlude);
+    }
+    return ans[0];
+    
+}
+
+
+
+int solveusingSpaceOptimization(int *arr,int length){
+    int prev = 0;
+    int next = arr[length-1];
+    int ans = 0;
+    for (int i = length-2; i >=0; i--)
+    {
+        int include = arr[i] + prev;
+        int exlude = 0 + next;
+        ans = max(include,exlude);
+        prev = next;
+        next = ans;
+    }
+
+    return ans ;
+    
+}
+
 
 
 int main(){
     int arr[] = {2,5,6,4,3,5};
-    int i = 0;
     int n = sizeof(arr)/sizeof(arr[0]);
-    vector<int> ans1(n+1,-1);
-    int ans = solve(arr,i,n,ans1);
-    cout<<ans<<endl;
+    // int ans = solve(arr,i,n,ans1);
+    int ans1 = solveusingSpaceOptimization(arr,n);
+    // cout<<ans1<<endl;
+    cout<<ans1;
     return 0;
 }
